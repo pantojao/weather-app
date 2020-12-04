7ed27ab881d0ad3e35f95c8ef99f9fdfcc2e2f52
pantojao <61061684+pantojao@users.noreply.github.com>
2020-12-04 14:03:05 -0800

    Updates

---
+++ precache-manifest.d0ca6ab72186f5667f268f6fa228da58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c7d956565e6e82808c25af28db37cd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6b79fe9ecb26d4d94a58",
+    "url": "/weather-app/static/css/main.284a1e8c.chunk.css"
+  },
+  {
+    "revision": "f1ebdf3c769cb7e09be9",
+    "url": "/weather-app/static/js/2.588b9019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.588b9019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b79fe9ecb26d4d94a58",
+    "url": "/weather-app/static/js/main.b0d8d799.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
